0bffa1e8ee6a509bad378a1847c1871343086e4f
Alex Shea <theredtexan@gmail.com>
2015-11-14 00:02:09 -0600

    Update ifctoday.py

--- ifctoday.py
+++ ifctoday.py
@@ -1,11 +1,19 @@
 import time
+
 ts = time.time()
 yearwoleaps = 1970 + (ts/31536000)
 numleapyears = int((yearwoleaps-1968)/4)
 yearswleaps = 1970 + (ts-(numleapyears*86400))/(31536000)
 wholeyears = int(yearswleaps)
 if int(yearswleaps) % 4 == 0:
-  julianday = int(366 * (yearswleaps-wholeyears)) 
+  julianday = int((366 * (yearswleaps-wholeyears))+1)
 else:
-  julianday = int(365 * (yearswleaps-wholeyears))
-print(julianday)
+  julianday = int((365 * (yearswleaps-wholeyears))+1)
+
+ifcmonthnum = int(julianday/28)
+ifcdaynum = int(julianday%28)
+
+ifcmonthnum = str(ifcmonthnum)
+ifcdaynum = str(ifcdaynum)
+wholeyears = str(wholeyears)
+print(ifcmonthnum + "-" + ifcdaynum + "-" + wholeyears)
